c4810517c7799d0902239b5a6b60d279a98c740f
Brandon <brandoncalitz52@gmail.com>
2024-04-25 18:21:36 +0200

    Auth Logic Done

--- app/src/main/java/com/timewise/timewise/Dashboard.kt
+++ app/src/main/java/com/timewise/timewise/Dashboard.kt
@@ -5,11 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class Dashboard : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        auth = Firebase.auth
+        val u = auth.currentUser?.email
         setContentView(R.layout.activity_dashboard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
