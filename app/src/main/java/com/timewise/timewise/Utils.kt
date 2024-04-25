d7339ed13af79b9098d3e300fe6ae8d9e1d6cbc6
Brandon <brandoncalitz52@gmail.com>
2024-04-25 18:21:36 +0200

    Auth Logic Done

--- app/src/main/java/com/timewise/timewise/Utils.kt
+++ app/src/main/java/com/timewise/timewise/Utils.kt
@@ -1,8 +1,13 @@
 package com.timewise.timewise
 
+import android.content.ContentValues.TAG
 import android.text.TextUtils
+import android.util.Log
 import android.util.Patterns
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
 
 public fun TextInputLayout.handleError(message: String?){
     if (message == null){
@@ -31,3 +36,26 @@ fun String.isLongEnough() = length >= 8
 fun String.hasEnoughDigits() = count(Char::isDigit) > 0
 fun String.isMixedCase() = any(Char::isLowerCase) && any(Char::isUpperCase)
 fun String.hasSpecialChar() = any { it in "@!,+^-$%&*#?" }
+
+public fun getUserDetails(id: String):User?{
+    val db = Firebase.firestore
+    var user:User? = null;
+    db.collection("users").whereEqualTo("fbUserId",id).get()
+        .addOnSuccessListener { documents ->
+        for (document in documents) {
+            user = document.toObject<User>()
+            return@addOnSuccessListener
+        }
+    }
+    return user
+
+}
+
+
+public data class User(
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val email: String? = null,
+    val fbUserId: String? = null,
+    val goal: Int? = null
+)
