5f4e1c5c5c5531560f9d5230454faac59347ea53
TobyDwyer <st10019602@vcconnect.edu.za>
2024-04-29 14:52:53 +0200

    added image function

--- app/src/main/java/com/timewise/timewise/TaskFragment.kt
+++ app/src/main/java/com/timewise/timewise/TaskFragment.kt
@@ -1,14 +1,23 @@
 package com.timewise.timewise
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.timewise.timewise.databinding.FragmentTaskBinding
+
 
 class TaskFragment : Fragment() {
     private lateinit var binding: FragmentTaskBinding
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<Intent>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,7 +28,29 @@ class TaskFragment : Fragment() {
         binding.leftButton.setOnClickListener{
             parentFragmentManager.popBackStack()
         }
+
+        binding.addAttachmentButton.setOnClickListener {
+            binding.addAttachmentButton.setOnClickListener {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                intent.type = "image/*"
+
+                imagePickerLauncher.launch(intent)
+            }
+        }
+
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                val selectedImageUri: Uri? = result.data?.data
+                binding.AttachmentImg.setImageURI(selectedImageUri)
+            }
+        }
+
+
+
+
         return view
     }
+
+
 
 }
